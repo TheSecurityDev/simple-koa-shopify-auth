8e76bbf73e1c5d4a4a9ac44b5ba758bdb56b75a5
TheSecurityDev <dt052901@gmail.com>
2022-09-27 12:01:09 -0500

    catch session errors and redirect to auth

--- src/verify-request.ts
+++ src/verify-request.ts
@@ -27,83 +27,85 @@ export default function verifyRequest(options?: VerifyRequestOptions) {
   };
 
   return async function verifyTokenMiddleware(ctx: Context, next: Next) {
-    const sessionData = await Shopify.Utils.loadCurrentSession(
-      ctx.req,
-      ctx.res,
-      accessMode === "online"
-    );
-    // Create session instance from loaded session data (if available), so we can call isActive() method on it
-    const session = sessionData ? Session.cloneSession(sessionData, sessionData.id) : null;
+    try {
+      const sessionData = await Shopify.Utils.loadCurrentSession(
+        ctx.req,
+        ctx.res,
+        accessMode === "online"
+      );
+      // Create session instance from loaded session data (if available), so we can call isActive() method on it
+      const session = sessionData ? Session.cloneSession(sessionData, sessionData.id) : null;
 
-    const { query, querystring } = ctx;
-    const shop = query.shop ? query.shop.toString() : "";
+      const { query, querystring } = ctx;
+      const shop = query.shop ? query.shop.toString() : "";
 
-    // Login again if the shops don't match
-    if (session && shop && session.shop !== shop) {
-      await clearSession(ctx, accessMode);
-      ctx.redirect(`${authRoute}?${querystring}`);
-      return;
-    }
+      // Login again if the shops don't match
+      if (session && shop && session.shop !== shop) {
+        await clearSession(ctx, accessMode);
+        ctx.redirect(`${authRoute}?${querystring}`);
+        return;
+      }
 
-    if (session) {
-      // Verify session is valid
-      try {
-        if (session.isActive()) {
-          // I think we need to verify on Shopify's side that the access token is valid, because otherwise anyone could just make their own 'valid' token and use it.
-          //   Of course if we're making requests to Shopify's API afterwords it will fail with an error, but we still need this check since we aren't always making a Shopify request when verifying this token (it might be an API request for our server, and we need to be sure it's the correct user).
-          // Make a request to make sure the token is valid on Shopify's end. If not, we'll get a 401 and have to re-authorize.
-          const client = new Shopify.Clients.Rest(session.shop, session.accessToken);
-          await client.get({ path: "shop" }); // Fetch /shop route on Shopify to verify the token is valid
-          setTopLevelOAuthCookieValue(ctx, null); // Clear the cookie
-          await next();
-          return;
-        }
-      } catch (err) {
-        if (
-          err instanceof HttpResponseError &&
-          ((err as any)?.code === 401 || err.response?.code === 401) // Shopify API v3+ uses 'response.code' instead of 'code'
-        ) {
-          // Session not valid, we will re-authorize
-        } else if (
-          err instanceof Shopify.Errors.InvalidJwtError ||
-          err instanceof Shopify.Errors.MissingJwtTokenError
-        ) {
-          // Invalid JWT session token, we will re-authorize (I don't know for sure if this error is thrown here)
-          console.warn("JWT token error in verifyRequest: " + err.message);
-        } else {
-          throw err;
+      if (session) {
+        // Verify session is valid
+        try {
+          if (session.isActive()) {
+            // I think we need to verify on Shopify's side that the access token is valid, because otherwise anyone could just make their own 'valid' token and use it.
+            //   Of course if we're making requests to Shopify's API afterwords it will fail with an error, but we still need this check since we aren't always making a Shopify request when verifying this token (it might be an API request for our server, and we need to be sure it's the correct user).
+            // Make a request to make sure the token is valid on Shopify's end. If not, we'll get a 401 and have to re-authorize.
+            const client = new Shopify.Clients.Rest(session.shop, session.accessToken);
+            await client.get({ path: "shop" }); // Fetch /shop route on Shopify to verify the token is valid
+            setTopLevelOAuthCookieValue(ctx, null); // Clear the cookie
+            await next();
+            return;
+          }
+        } catch (err) {
+          if (
+            err instanceof HttpResponseError &&
+            ((err as any)?.code === 401 || err.response?.code === 401) // Shopify API v3+ uses 'response.code' instead of 'code'
+          ) {
+            // Session not valid, we will re-authorize
+          } else {
+            throw err;
+          }
         }
       }
-    }
 
-    // ! If we get here, either the session is invalid or we need to re-authorize
+      // ! If we get here, either the session is invalid or we need to re-authorize
 
-    // We need to re-authenticate
-    if (returnHeader) {
-      // Return a header to the client so they can re-authorize
-      ctx.response.status = 401;
-      ctx.response.set(REAUTH_HEADER, "1"); // Tell the client to re-authorize by setting the reauth header
-      // Get the shop from the session, or the auth header (we can't get it from the query if we're making a post request)
-      let reauthUrl = authRoute;
-      try {
+      // We need to re-authenticate
+      if (returnHeader) {
+        // Return a header to the client so they can re-authorize
+        ctx.response.status = 401;
+        ctx.response.set(REAUTH_HEADER, "1"); // Tell the client to re-authorize by setting the reauth header
+        // Get the shop from the session, or the auth header (we can't get it from the query if we're making a post request)
+        let reauthUrl = authRoute;
         if (Shopify.Context.IS_EMBEDDED_APP) {
           reauthUrl += `?${getShopAndHostQueryStringFromAuthHeader(ctx)}`;
         } else {
           reauthUrl += `?${new URL(ctx.header.referer).search}`; // Get parameters from the referer header (not completely sure if this will work)
         }
-      } catch (err) {
-        if (
-          err instanceof Shopify.Errors.InvalidJwtError ||
-          err instanceof Shopify.Errors.MissingJwtTokenError
-        ) {
-          console.warn("JWT token error in verifyRequest: " + err.message);
-        }
+        ctx.response.set(REAUTH_URL_HEADER, reauthUrl); // Set the reauth url header
+      } else {
+        // Otherwise redirect to the auth page
+        ctx.redirect(`${authRoute}?${querystring}`);
+      }
+
+      // Catch session errors and redirect to auth page
+    } catch (err) {
+      if (
+        err instanceof Shopify.Errors.InvalidJwtError ||
+        err instanceof Shopify.Errors.MissingJwtTokenError ||
+        err instanceof Shopify.Errors.SessionNotFound
+      ) {
+        console.warn(err.message);
+        // If the session is invalid, clear the session and redirect to the auth route
+        await clearSession(ctx, accessMode);
+        ctx.redirect(`${authRoute}?${getShopAndHostQueryStringFromAuthHeader(ctx)}`);
+        return;
+      } else {
         throw err;
       }
-      ctx.response.set(REAUTH_URL_HEADER, reauthUrl); // Set the reauth url header
-    } else {
-      // Otherwise redirect to the auth page
-      ctx.redirect(`${authRoute}?${querystring}`);
     }
   };
 }
